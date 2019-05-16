39e8c325d69eaf41aa85717c8d182d70fda0aab4
shahaf1312 <shahaf.her@gmail.com>
2019-05-17 02:07:41 +0300

    By Shahaf: updated map button. added app logo. 02:07  issue: crashing on last screen, needs an activity to go to

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hackathon2019;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +13,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button questionsActivityBtn = (Button) findViewById(R.id.goToQuestion);
-        questionsActivityBtn.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), QuestionActivity.class);
-                startActivity(startIntent);
+            public void run() {
+
+                Intent i = new Intent(MainActivity.this, QuestionActivity.class);
+                startActivity(i);
             }
-        });
+        }, 3000);
+
+//        Button questionsActivityBtn = (Button) findViewById(R.id.goToQuestion);
+//        questionsActivityBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent startIntent = new Intent(getApplicationContext(), QuestionActivity.class);
+//                startActivity(startIntent);
+//            }
+//        });
     }
 }
