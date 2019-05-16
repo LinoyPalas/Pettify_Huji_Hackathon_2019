4c0fb68d0faf73983a42a5092398df1519e11ccc
shahaf1312 <shahaf.her@gmail.com>
2019-05-17 01:43:27 +0300

    By Shahaf: changed font, 01:43  issue: crashing on last screen

--- app/src/main/java/com/example/hackathon2019/QuestionActivity.java
+++ app/src/main/java/com/example/hackathon2019/QuestionActivity.java
@@ -50,9 +50,6 @@ public class QuestionActivity extends AppCompatActivity {
 
         title = findViewById(R.id.titleTextView);
         question = findViewById(R.id.questionTextView);
-//        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/OSAran_W_500FFC.ttf");
-//        title.setTypeface(custom_font);
-//        question.setTypeface(custom_font);
 
         signupFlipper = findViewById(R.id.signup_flipper);
 
