320b2712e513377b91fef334667cd3d474023363
OmerLiberman <omer.liberman@mail.huji.ac.il>
2019-05-16 15:11:26 +0300

    Omer's commit

--- app/src/main/java/com/example/hackathon2019/MainActivity.java
+++ app/src/main/java/com/example/hackathon2019/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hackathon2019;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
